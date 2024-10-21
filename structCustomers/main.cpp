e5fcd0cde1748ff5586b210b957263074e3ae314
Sonia Lopchan <30045332@cc.peralta.edu>
2024-10-21 10:37:13 -0700

    Initial Commit

---
+++ structCustomers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  structCustomers
+//
+//  Created by Sonia Lopchan on 10/21/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
