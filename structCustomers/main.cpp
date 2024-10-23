2ae08da8a1f7b5573fc0940ec946691b4e08208b
Sonia Lopchan <30045332@cc.peralta.edu>
2024-10-23 15:57:02 -0700

    main.cpp

--- structCustomers/main.cpp
+++ structCustomers/main.cpp
@@ -4,11 +4,31 @@
 //
 //  Created by Sonia Lopchan on 10/21/24.
 //
-
 #include <iostream>
+#include <string>
+#include "Customer.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(){
+    
+    Customer customer;// Customer data type, customer variable
+    
+    cout <<"Enter customer ID: ";
+    cin >> customer.id;
+
+    cout <<"Enter name: ";
+    cin.ignore();
+    getline(cin,customer.name);
+    
+    cout <<"Enter email address: ";
+    cin >> customer.emailAddress;
+    
+    //outputs in order 
+    cout<<customer.id<<"\n";
+    cout<<customer.name<<"\n";
+    cout<<customer.emailAddress<<endl;
+
     return 0;
 }
+
