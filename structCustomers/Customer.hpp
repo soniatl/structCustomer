35fa773a62450738e15c705c077db251a02ca3ff
Sonia Lopchan <30045332@cc.peralta.edu>
2024-10-23 15:54:05 -0700

    Customer.hpp

---
+++ structCustomers/Customer.hpp
@@ -0,0 +1,15 @@
+//
+//  Customer.h
+//  structCustomers
+//
+//  Created by Sonia Lopchan on 10/23/24.
+//
+
+using namespace std;
+#include <string>
+
+struct Customer { //creating a structure named Customer
+    int id;
+    string name, emailAddress;
+};
+
